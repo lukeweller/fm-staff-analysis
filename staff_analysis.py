029bf4d8c6a89641fe97fffb0fffa46b2b2fd768
lukeweller <lukeweller8@gmail.com>
2023-02-15 17:40:42 -0500

    corrected formula for coaching aptitude analysis

--- staff_analysis.py
+++ staff_analysis.py
@@ -3,19 +3,22 @@ import os, sys, time
 import pandas as pd
 
 # Save a default filename for the input
-DEFAULT_INPUT_FILENAME = './input/rtf/coaching_candidates.rtf'
+DEFAULT_INPUT_FILENAME = './input/coaching_candidates.rtf'
 
-# Sets default # of top canidates to print
+# Sets default # of top candidates to print
 DEFAULT_PRINT_NO = 10
 
-ATTRIBUTES 					= ['Ada', 'Att', 'Def', 'Det', 'Fit', 'GkD', 'GkH', 'GkS', 'Judge A', 'Jud PD', 'Judge P', 'Jud SA', 'Jud TD', 'Dis', 'Man', 'Men', 'Mot', 'Negotiating', 'Phy', 'Prs D', 'SpS', 'TCo', 'Tac Knw', 'Tec', 'Youth']
-COACHING_ATTRIBUTES 		= ['Att', 'Def', 'Men', 'Tec', 'TCo', 'Det', 'Dis', 'Mot']
-GK_COACHING_ATTRIBUTES 		= ['GkD', 'GkH', 'GkS', 'Det', 'Dis', 'Mot']
-FITNESS_COACHING_ATTRIBUTES = ['Fit', 'Det', 'Dis', 'Mot']
+MENTAL_COACHING_ATTRIBUTES  = ['Det', 'Dis', 'Mot']
+COACHING_ATTRIBUTES 		= ['Att', 'Def', 'Men', 'Tec', 'TCo'] + MENTAL_COACHING_ATTRIBUTES
+GK_COACHING_ATTRIBUTES 		= ['GkD', 'GkH', 'GkS'] + MENTAL_COACHING_ATTRIBUTES
+FITNESS_COACHING_ATTRIBUTES = ['Fit'] + MENTAL_COACHING_ATTRIBUTES
 HEAD_YOUTH_DEV_ATTRIBUTES 	= ['Judge A', 'Judge P', 'Youth']
 HEAD_COACH_ATTRIBUTES 	    = ['Man', 'Mot', 'Judge A', 'Judge P', 'Tac Knw']
 
-COACHING_APTITUDES 			= ['Att-Tec', 'Att-TCo', 'Def-Tec', 'Def-TCo', 'Men-Tec', 'Men-TCo']
+# Duplicate-free list of all possible attributes (columns) for use in preprocessing()
+ALL_ATTRIBUTES = list(set(COACHING_ATTRIBUTES + GK_COACHING_ATTRIBUTES + FITNESS_COACHING_ATTRIBUTES + HEAD_YOUTH_DEV_ATTRIBUTES + HEAD_COACH_ATTRIBUTES))
+
+COACHING_APTITUDES = ['Att-Tec', 'Att-TCo', 'Def-Tec', 'Def-TCo', 'Men-Tec', 'Men-TCo']
 
 # Helper function for skiprows parameter in pandas.read_csv()
 # Returns True for all lines numbers to read & returns False for all line numbers to skip
@@ -25,18 +28,11 @@ def skip_rows(x):
 	else:
 		return True
 
-# Simple wrapper function that calls load_staff_rtf() or load_staff_csv(), depending on state
-def load_staff(input_filename, caching):
-
-	cache_filename = './input/csv/' + input_filename[12:-4] + '_cleaned.csv'
-
-	if caching and os.path.exists(cache_filename):
-		df = load_staff_csv(cache_filename)
-	else:
-		df = load_staff_rtf(input_filename)
-		df = preprocessing(df)
-		if caching:
-			write_df_to_csv(df, cache_filename)
+# Wrapper function that calls load_staff_rtf() and preprocessing()
+def load_staff(input_filename):
+	
+	df = load_staff_rtf(input_filename)
+	df = preprocessing(df)
 
 	return df
 
@@ -47,23 +43,6 @@ def load_staff_rtf(input_filename):
 
 	except:
 		print('Error: failed to load input file \'{}\' into dataframe'.format(input_filename))
-		sys.exit(1)
-
-
-	return df
-
-def load_staff_csv(input_filename):
-
-	load_csv_start_time = time.time()
-
-	try:
-		df = pd.read_csv(input_filename)
-
-		print('time to load \'{}\' into dataframe: {:.3f}s'.format(input_filename, time.time() - load_csv_start_time))
-	
-	except:
-		print('error: failed to load input file \'./{}\' into the df'.format(input_filename))
-		
 		sys.exit(1)
 
 	return df
@@ -83,37 +62,21 @@ def preprocessing(df):
 	df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
 	for col in df.columns:
-		if col in ATTRIBUTES:
+		if col in ALL_ATTRIBUTES:
 			df[col] = pd.to_numeric(df[col])
 
 	print('total preprocessing time: {:.3f}s'.format(time.time() - preprocessing_start_time))
 
 	return df
 
-def write_df_to_csv(df, cache_filename):
-
-	write_df_to_csv_start_time = time.time()
-
-	df.to_csv(cache_filename, index=False)
-
-	print('time to write cleaned data to file: {:.3f}s'.format(time.time() - write_df_to_csv_start_time))
-
-def overall_analysis(df):
-
-	df['sum_all_attributes'] = sum(df[attribute] for attribute in ATTRIBUTES)
-
-	df = df.sort_values(by='sum_all_attributes', ascending=False)
-
-	return df
-
-def coaching_analysis(df, sort_by='max_coaching_aptitude'):
+def coaching_analysis(df, sort_by):
+	# The formula for determining coach aptitude (as seen in Training -> Coaches) is:
+	# 6 * [Attack, Defense, Mental] + 3 * [Technical, Tactical] + 2 * [Determination + Discipline + Motivation]
+	# The max possible aptitude is 300.  Aptitudes > 270 = 5 stars, aptitudes > 240 < 270 = 4.5 stars, etc.
 	
-	# df['mental_sum'] = df['Det'] + df['Dis'] + df['Mot']
-	df['mental_sum'] = round(((df['Det'] + df['Dis'] + df['Mot']) / 2), 2)
-
 	for coaching_area in ['Att', 'Def', 'Men']:
 		for coaching_style in ['Tec', 'TCo']:
-			df[coaching_area + '-' + coaching_style] = df['mental_sum'] + df[coaching_area] + df[coaching_style]
+			df[coaching_area + '-' + coaching_style] = 2 * sum(df[_] for _ in MENTAL_COACHING_ATTRIBUTES) + 6 * df[coaching_area] + 3 * df[coaching_style]
 
 	df['max_coaching_aptitude'] = df[COACHING_APTITUDES].max(axis=1)
 	df['total_coaching_aptitude'] = sum(df[_] for _ in COACHING_APTITUDES)
@@ -171,18 +134,16 @@ def print_help_msg(exit_status):
 	print('Usage:\n'
 		  '	1. Import custom view file (`./views/All.fmf`) into the \'Staff Search\' page while in FM\n'
 		  '	2. Copy all view data (ctrl-A, ctrl-P) and save data as a text file to `./input/All.rtf`\n'
-		  '	3. Use `./staff-search.py` to analyze coaches and print top canidates\n'
+		  '	3. Use `./staff-search.py` to analyze coaches and print top candidates\n'
 		  'Options:\n'
 		  '	-i, --input\n'
 		  '		specify the name of the input file; e.g., ./staff-search.py -i [filename]\n'
 		  '	-n, --number\n'
-		  '		specify number of top canidates to print; e.g., ./staff-search.py -n [number]\n'
-		  '	-e, --caching\n'
-		  '		enables caching for faster processing times on subsequent runs\n'
-		  '	-o, --overall\n'
-		  '		sort coaches by sum of all attributes; enabled by default if no analysis type (e.g. coaching) is selected\n'
+		  '		specify number of top candidates to print; e.g., ./staff-search.py -n [number]\n'
+		  ' -s, --sort-by\n'
+		  '		specify an attribute to sort '
 		  '	-c, --coaching\n'
-		  '		sort coaches by their max coaching aptitude\n'
+		  '		sort coaches by their max coaching aptitude; enabled by default if no analysis type is selected\n'
 		  ' -tc, --total-coaching\n'
 		  '		sort coaches by their total coaching aptitude\n'
 		  '	-gk, --goalkeeper\n'
@@ -202,9 +163,8 @@ if __name__ == '__main__':
 
 	input_filename = DEFAULT_INPUT_FILENAME
 	no_candidates = DEFAULT_PRINT_NO
-	caching = False
 	staff_role = ''
-	sort_by = 'max_coaching_aptitude'
+	sort_by = ''
 
 	# Iterate through args
 	while len(args) > 0:
@@ -215,12 +175,11 @@ if __name__ == '__main__':
 			input_filename = args.pop(0)
 		elif arg == '-n' or arg == '--number':
 			no_candidates = int(args.pop(0))
-		elif arg == '-e' or arg == '-enable-caching':
-			caching = True
-		elif arg == '-o' or arg == '--overall':
-			staff_role = 'overall'
+		elif arg == '-s' or arg == '--sort-by':
+			sort_by = args.pop(0)
 		elif arg == '-c' or arg =='--coaching':
 			staff_role = 'coaching'
+			sort_by = 'max_coaching_aptitude'
 		elif arg == '-tc' or arg =='--total-coaching':
 			staff_role = 'coaching'
 			sort_by = 'total_coaching_aptitude'
@@ -236,12 +195,9 @@ if __name__ == '__main__':
 			print('error: failed to recognize argument \'{}\' while parsing command-line arguments'.format(arg))
 			print_help_msg(1)
 
-	df = load_staff(input_filename, caching)
+	df = load_staff(input_filename)
 
-	if staff_role == 'overall' or staff_role == '':
-		df = overall_analysis(df)
-		print_top_candidates(df, no_candidates, ATTRIBUTES + ['sum_all_attributes'])
-	elif staff_role == 'coaching':
+	if staff_role == 'coaching' or staff_role == '':
 		df = coaching_analysis(df, sort_by)
 		print_top_candidates(df, no_candidates, COACHING_APTITUDES + ['max_coaching_aptitude', 'total_coaching_aptitude'])
 	elif staff_role == 'goalkeeper_coaching':
@@ -256,3 +212,8 @@ if __name__ == '__main__':
 	elif staff_role == 'head_coach':
 		df = head_coach_analysis(df)
 		print_top_candidates(df, no_candidates, HEAD_COACH_ATTRIBUTES + ['Preferred Formation', 'Tactical Style', 'head_coach_aptitude'])
+
+
+	# Web Output Testing
+	result = df.head(no_candidates).to_html()
+	# print(result)
